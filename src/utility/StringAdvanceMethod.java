f11ecd508bb29ae22dc0e83b113117f0ed5a26ae
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-18 21:56:54 +0530

    Add files via upload

--- src/utility/StringAdvanceMethod.java
+++ src/utility/StringAdvanceMethod.java
@@ -1,3 +1,35 @@
 package utility;
 
 // Type your code
+
+public class StringAdvanceMethod{
+	private String str1;
+	private String str2;
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+	public String getStr2() {
+		return str2;
+	}
+	public void setStr2(String str2) {
+		this.str2 = str2;
+	}
+	
+	public String concat(String s1,String s2)
+	{
+		return s1+s2;
+	}
+	
+	public String splitString(String s1,String s2)
+	{
+		return s1.split(s2,1).toString();
+	}
+	public String index(String s1,String s2)
+	{
+		int i = s1.indexOf(s2);
+		return Integer.toString(i);
+	}
+}
--- src/utility/StringMethod.java
+++ src/utility/StringMethod.java
@@ -1,3 +1,40 @@
 package utility;
 
 // Type your code
+
+public class StringMethod{
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+	
+	public String lower(String s)
+	{
+		return s.toLowerCase();
+	}
+	public String upper(String s)
+	{
+		return s.toUpperCase();
+	}
+	
+	public String firstcharacter(String s)
+	{
+		char ch = s.charAt(0);
+		return Character.toString(ch);
+	}
+	public String lengthString(String s)
+	{
+		int len = s.length();
+		return Integer.toString(len);
+	}
+	public String trimString(String s)
+	{
+		return s.trim();
+		
+	}
+}
